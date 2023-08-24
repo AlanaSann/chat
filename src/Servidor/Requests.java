9f77de03393d326a130118837683f0a53684dbd7
Ntheus <matheusoliveira.7313@gmail.com>
2023-08-24 16:27:01 -0300

    fucoes de servidor modularizadas

--- src/Servidor/Requests.java
+++ src/Servidor/Requests.java
@@ -29,21 +29,29 @@ public class Requests implements Runnable {
     public void run() {
         try (BufferedReader leitor = new BufferedReader(new InputStreamReader(remetente.getInputStream()))) {
             String mensagem = leitor.readLine();
-            for (Map.Entry<String, Socket> pair : clients.entrySet()) {
-                try (Socket conection = new Socket(pair.getValue().getInetAddress().getHostAddress(), 4321);) {
-                    PrintWriter escritor = new PrintWriter(conection.getOutputStream(), true);
-                    System.out.println("Reenviando mensagem: " + mensagem + " para" + pair.getValue().getInetAddress());
-                    escritor.println(mensagem);
-                } catch (Exception e) {
-                    System.out.println(clients.size());
-                    clients.remove(pair.getKey());
-                    System.out.println(clients.size());
-                }
-            }
             System.out.println(clients.size());
+            enviaMensagensParaTodosOsUsuariosDaLista(mensagem);
         } catch (IOException e) {
-
             e.printStackTrace();
+        }
+    }
+
+    public void enviaMensagensParaTodosOsUsuariosDaLista(String mensagem) {
+        for (Map.Entry<String, Socket> pair : clients.entrySet()) {
+            enviaMensagensParaUmUsuario(mensagem, pair.getValue());
+        }
+    }
+
+    public void enviaMensagensParaUmUsuario(String mensagem, Socket socketDeUmUsuario) {
+        try (Socket conection = new Socket(socketDeUmUsuario.getInetAddress().getHostAddress(), 4321);) {
+            PrintWriter escritor = new PrintWriter(conection.getOutputStream(), true);
+            System.out.println(
+                    "Reenviando mensagem: " + mensagem + " para" + socketDeUmUsuario.getInetAddress());
+            escritor.println(mensagem);
+        } catch (Exception e) {
+            System.out.println(clients.size());
+            clients.remove(socketDeUmUsuario.getInetAddress().getCanonicalHostName());
+            System.out.println(clients.size());
         }
     }
 }
